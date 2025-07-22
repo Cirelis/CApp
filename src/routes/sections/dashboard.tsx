dc434fae0ead7631484f0b47c6bcf6e9fa0fd491
Gumo4P <timo.riesenberger@gmail.com>
2025-07-22 13:32:51 +0200

    cleanup

--- src/routes/sections/dashboard.tsx
+++ src/routes/sections/dashboard.tsx
@@ -40,7 +40,8 @@ export const dashboardRoutes: RouteObject[] = [
     path: 'dashboard',
     element: CONFIG.auth.skip ? dashboardLayout() : <AuthGuard>{dashboardLayout()}</AuthGuard>,
     children: [
-
+      { index: true, element: <IndexPage /> },
+      // { path: 'ecommerce', element: <OverviewEcommercePage /> },
     ],
   },
 ];
